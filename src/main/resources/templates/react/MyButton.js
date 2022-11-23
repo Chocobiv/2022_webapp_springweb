b5994c9b46485d0cd977b7b72dec62dd0e12b7ab
Chocobiv <chocobiv123@gmail.com>
2022-11-23 12:18:15 +0900

    74일차_react cdn 가상 dom 실습

--- src/main/resources/templates/react/MyButton.js
+++ src/main/resources/templates/react/MyButton.js
@@ -1,7 +1,9 @@
 
 //1. root라는 id를 갖는 html 태그 호출
 const domContainer = document.querySelector('#root')
-//2. 리액트 렌더링
+console.log(domContainer)
+
+//2. 리액트 렌더링 [ render() ]
 ReactDOM.render(ReactDOM.createElement(MyButton), domContainer)
 
 //3. MyButton 생성 함수
