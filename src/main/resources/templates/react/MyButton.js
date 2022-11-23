c92349c60d20f1c7259e83f9b4e3de74f229fb56
Chocobiv <chocobiv123@gmail.com>
2022-11-23 12:16:48 +0900

    74일차_react cdn 실습

---
+++ src/main/resources/templates/react/MyButton.js
@@ -0,0 +1,16 @@
+
+//1. root라는 id를 갖는 html 태그 호출
+const domContainer = document.querySelector('#root')
+//2. 리액트 렌더링
+ReactDOM.render(ReactDOM.createElement(MyButton), domContainer)
+
+//3. MyButton 생성 함수
+function MyButton(props) {  //props : properties약자
+    const[isClicked,setIsClicked] = ReactDOM.useState(false)
+    return React.createElement(
+        'button',
+        {onClick: () => setIsClicked(true)},
+        isClicked ? 'Clicked!' : 'Click here!'
+    )
+}
+
