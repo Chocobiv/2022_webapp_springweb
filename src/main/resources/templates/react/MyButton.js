feaee0789218abf5b266124137717aa674ac8257
Chocobiv <chocobiv123@gmail.com>
2022-11-23 13:31:18 +0900

    74일차_react cdn 가상 dom 실습

--- src/main/resources/templates/react/MyButton.js
+++ src/main/resources/templates/react/MyButton.js
@@ -3,16 +3,16 @@
 const domContainer = document.querySelector('#root')
 console.log(domContainer)
 
-//2. 리액트 렌더링 [ render() ]
-ReactDOM.render(ReactDOM.createElement(MyButton), domContainer)
+//2. 리액트 렌더링 [ render(이벤트,위치) ]
+ReactDOM.render(React.createElement(MyButton), domContainer)
 
 //3. MyButton 생성 함수
 function MyButton(props) {  //props : properties약자
-    const[isClicked,setIsClicked] = ReactDOM.useState(false)
-    return React.createElement(
-        'button',
-        {onClick: () => setIsClicked(true)},
-        isClicked ? 'Clicked!' : 'Click here!'
+    const[isClicked,setIsClicked] = React.useState(false)
+    return React.createElement( //React.createElement( <button> )
+        'button',   //태그명
+        {onClick: () => setIsClicked(true)},    //옵션 : 이벤트
+        isClicked ? 'Clicked!' : 'Click here!'  //html 작성
     )
 }
 
