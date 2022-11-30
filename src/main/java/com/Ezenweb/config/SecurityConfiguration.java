b24218fe990df3fd87a174f8205312311d35f7dc
chocobiv <chocobiv123@gmail.com>
2022-11-30 09:51:34 +0900

    76day_Spring Security

--- src/main/java/com/Ezenweb/config/SecurityConfiguration.java
+++ src/main/java/com/Ezenweb/config/SecurityConfiguration.java
@@ -9,7 +9,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override   //재정의 [상속받은 클래스로부터 메소드 재구현]
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);  //super : 상속클래스     // 기본값 : 모든 HTTP 보안 설정
-
     }
 
 }
