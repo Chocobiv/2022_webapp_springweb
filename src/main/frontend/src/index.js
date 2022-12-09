7e7f1c451d4d276855a788e3bb1cc70b42b1e215
chocobiv <chocobiv123@gmail.com>
2022-12-09 12:09:09 +0900

    Day86_react chapter11 폼

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -6,18 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 
 import Index from './component/Index'
-import Signup from "./component/member/Signup";
 // 1. 사용할 컴포넌트 호출 [import 컴포넌트명 from 위치]
-import Library from "./Book/chapter3/Library";                      //3장
-import Clock from "./Book/chapter4/Clock";                          //4장
-import CommentList from "./Book/chapter5/CommentList";              //5장
-import NotificationList from "./Book/chapter6/NotificationList";    //6장
-import Counter from "./Book/chapter7/Ex1_Hook";                     //7장
-import Accommodate from "./Book/chapter7/Accommodate";              //7장
-import ConfirmButton from "./Book/chapter8/ConfirmButton";          //8장
-import ConfirmButton2 from "./Book/chapter8/ConfirmButton2";        //8장
-import TestState from "./Book/chapter8/TestState";                  //8장
-import LandingPage from "./Book/chapter9/LandingPage";              //9장
+import Signup from "./component/member/Signup";
 
 // 2. DOM 컨테이너 뿌릴 위치 [public-index.html 안에 있는 태그]
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -36,83 +26,7 @@ root.render(
   </React.StrictMode>
 );*/
 
-//2. [Library 컴포넌트를 root에 렌더링]
-/*root.render(
-    <React.StrictMode>
-        <Library />
-    </React.StrictMode>
-);*/
 
-//3. [Clcok 컴포넌트를 root에 렌더링]
-//setInterval( (인수) => {실행문}, 밀리초) :: 1초마다 렌더링
-/*setInterval( () => {
-    root.render(
-        <React.StrictMode>
-            <Clock />
-        </React.StrictMode>
-    );
-}, 1000)*/
-
-//4. [CommentList 컴포넌트를 root에 렌더링]
-/*root.render(
-    <React.StrictMode>
-        <CommentList />
-    </React.StrictMode>
-);*/
-
-//5.
-/*root.render(
-    <React.StrictMode>
-        <Signup />
-    </React.StrictMode>
-);*/
-
-//6.
-/*root.render(
-    <React.StrictMode>
-        <NotificationList />
-    </React.StrictMode>
-);*/
-
-//7. ex1
-/*root.render(
-    <React.StrictMode>
-        <Counter />
-    </React.StrictMode>
-);*/
-
-//7. 실습
-/*root.render(
-    <React.StrictMode>
-        <Accommodate />
-    </React.StrictMode>
-);*/
-
-//8.
-/*root.render(
-    <React.StrictMode>
-        <ConfirmButton />
-    </React.StrictMode>
-);*/
-//8. 실습
-/*root.render(
-    <React.StrictMode>
-        <ConfirmButton2 />
-    </React.StrictMode>
-);*/
-
-/*root.render(
-    <React.StrictMode>
-        <TestState />
-    </React.StrictMode>
-);*/
-
-//9. 실습
-/*root.render(
-    <React.StrictMode>
-        <LandingPage />
-    </React.StrictMode>
-);*/
 
 
 // If you want to start measuring performance in your app, pass a function
