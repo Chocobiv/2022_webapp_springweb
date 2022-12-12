079d78292a2d888b119dc3075d2b05c99d62333f
chocobiv <chocobiv123@gmail.com>
2022-12-12 13:19:00 +0900

    Day87_페이징 처리 API

--- src/main/frontend/src/component/board/BoardList.jsx
+++ src/main/frontend/src/component/board/BoardList.jsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import Pagenation from 'react-js-pagination'
 
 export default function BoardList(props){
     //1. 메모리
     const [pageInfo, setPageInfo] = useState({bcno:0,page:1,key:"",keyword:""})     //1. 요청 정보 객체 state
     const [boardlist, setBoardlist] = useState({list:[]})   //1. 게시물 리스트 state
-    const [categorylist, setCategoryList] = useState([])   //1. 게시물 리스트 state
+    const [categorylist, setCategoryList] = useState([])   //1. 카테고리 리스트 state
 
+    // ---------------------------- 1. 게시물 ---------------------------- //
     //2. 서버로부터 게시물 리스트를 가져오는 함수 [실행조건 :: 1.렌더링될때 2.검색할때 3.카테고리선택 4.페이징선택 -> 일반 함수화]
     function getboardlist() {
         axios.post("/board/boardlist", pageInfo)
@@ -16,6 +18,7 @@ export default function BoardList(props){
     //3. 렌더링 될때 그리고 pageInfo 변경될 때마다 작동
     useEffect(getboardlist, [pageInfo])
 
+    // ---------------------------- 2. 카테고리 ---------------------------- //
     function getBcategorylist() {    //4. 모든 카테고리 가져오기
         axios.post("/board/bcategorylist", pageInfo)
             .then(res => { setCategoryList(res.data); })
@@ -25,6 +28,10 @@ export default function BoardList(props){
 
     //카테고리 버튼을 선택했을 때
     const onCategory = (bcno) => { setPageInfo({bcno:bcno,page:1,key:"",keyword:""}) }
+
+    // ---------------------------- 3. 페이징 ---------------------------- //
+    const onPage = (page) => { setPageInfo({bcno:pageInfo.bcno, page:page,key:"",keyword:""})}//pageInfo.bcno : 기존 카테고리
+
 
     return (
         <div>
@@ -53,6 +60,14 @@ export default function BoardList(props){
                     })
                 }
             </table>
+            <Pagenation
+                activePage={pageInfo.page}
+                itemsCountPerPage={3}
+                totalItemsCount={boardlist.totalBoards}
+                pageRangeDisplayed={5}
+                onChange={onPage}
+            />
+
         </div>
     )
 }
